88d6efd04998022999e4294f73966e279e5e8457
gpnt <ff25tj@gmail.com>
2018-08-30 21:23:13 +0300

    add base classes #15

---
+++ logistics/src/main/java/com/alevel/deliverit/ModuleAPI.java
@@ -0,0 +1,9 @@
+package com.alevel.deliverit;
+
+public class ModuleAPI {
+    private final BuisenesLogickService buisenesLogickService;
+
+    public ModuleAPI(BuisenesLogickService buisenesLogickService) {
+        this.buisenesLogickService = buisenesLogickService;
+    }
+}
