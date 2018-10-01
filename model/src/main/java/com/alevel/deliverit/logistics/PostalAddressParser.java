30bb4f4d07d6ad7db3e3a2740cf5df63d44f3313
Leowario <voronventusa@gmail.com>
2018-10-01 18:11:19 +0300

    Fixed conflicts

--- model/src/main/java/com/alevel/deliverit/logistics/PostalAddressParser.java
+++ model/src/main/java/com/alevel/deliverit/logistics/PostalAddressParser.java
@@ -17,8 +17,23 @@ public class PostalAddressParser extends Parser<PostalAddress> {
 
         JSONObject jsonObject = parseToJsonObject(jsonSting);
         String countryJson = jsonObject.get("country").toString();
+        String firstName = (String) jsonObject.get("firstName");
+        String lastName = (String) jsonObject.get("lastName");
+        String houseNumber = (String) jsonObject.get("houseNumber");
+        String street = (String) jsonObject.get("street");
+        String apartmentNumber = (String) jsonObject.get("apartmentNumber");
+        String city = (String) jsonObject.get("city");
+        String postalCode = (String) jsonObject.get("postalCode");
         Country country = Country.parser().parse(countryJson);
-        String address = (String) jsonObject.get("address");
-        return new PostalAddress(country, address);
+        return  PostalAddress.builder()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setHouseNumber(houseNumber)
+                .setCountry(country)
+                .setCity(city)
+                .setApartmentNumber(apartmentNumber)
+                .setPostalCode(postalCode)
+                .setStreet(street)
+                .build();
     }
 }
