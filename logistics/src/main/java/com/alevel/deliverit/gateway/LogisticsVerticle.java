ff5edb95d0b15372d6fe1f12c32010907caed09a
Vladyslav Lubenskyi <vladyslav.lubenskyi@teamdev.com>
2018-10-24 00:01:32 +0300

    Make RestVerticle async

--- logistics/src/main/java/com/alevel/deliverit/gateway/LogisticsVerticle.java
+++ logistics/src/main/java/com/alevel/deliverit/gateway/LogisticsVerticle.java
@@ -2,14 +2,10 @@ package com.alevel.deliverit.gateway;
 
 import com.alevel.deliverit.ModuleAPI;
 import com.alevel.deliverit.ServiceMethod;
-import com.alevel.deliverit.codecs.DefaultCodec;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.eventbus.MessageCodec;
 
-import java.io.*;
 import java.util.Map;
 import java.util.Optional;
 
@@ -17,63 +13,18 @@ import java.util.Optional;
  * @author Sergey Bogovesov
  */
 public class LogisticsVerticle extends AbstractVerticle {
-    private DefaultCodec defaultCodec = new DefaultCodec();
     private DeliveryOptions options;
 
     public LogisticsVerticle() {
         init();
-        options = new DeliveryOptions().setCodecName(defaultCodec.name());
+        options = new DeliveryOptions().setCodecName(ByteArrayCodec.CODEC_NAME);
     }
 
     @Override
-    public void start() throws Exception {
+    public void start() {
         Map<String, ServiceMethod> consumersContainer = ModuleAPI.getInstance().getConsumersContainer();
 
-        EventBus eb = vertx.eventBus();
-        eb.registerCodec(new MessageCodec() {
-            @Override
-            public void encodeToWire(Buffer buffer, Object o) {
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                try (ObjectOutputStream out = new ObjectOutputStream(bos)) {
-                    out.writeObject(o);
-                    out.flush();
-                    final byte[] bytes = bos.toByteArray();
-                    buffer.appendBytes(bytes);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public Object decodeFromWire(int pos, Buffer buffer) {
-                ByteArrayInputStream bis = new ByteArrayInputStream(buffer.getBytes(pos, buffer.length()));
-                ObjectInput in = null;
-                try {
-                    in = new ObjectInputStream(bis);
-                    Object o = in.readObject();
-                    return o;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    return null;
-                }
-            }
-
-            @Override
-            public Object transform(Object o) {
-                return o;
-            }
-
-            @Override
-            public String name() {
-                return "myCodec";
-            }
-
-            @Override
-            public byte systemCodecID() {
-                return -1;
-            }
-        });
+        EventBus eb = getVertx().eventBus();
 
         for (Map.Entry<String, ServiceMethod> e : consumersContainer.entrySet()) {
 
@@ -93,7 +44,7 @@ public class LogisticsVerticle extends AbstractVerticle {
 
     private void init() {
         ModuleAPI.getInstance().registerConsumers(new RouteLookup());
-//        ModuleAPI.getInstance().registerConsumers(new PriceLookup());
+        ModuleAPI.getInstance().registerConsumers(new PriceLookup());
         ModuleAPI.getInstance().registerConsumers(new DeliveryTimeLookup());
         ModuleAPI.getInstance().registerConsumers(new TrackNumberLookup());
     }
--- rest-api/src/main/java/com/alevel/deliverit/RestVerticle.java
+++ rest-api/src/main/java/com/alevel/deliverit/RestVerticle.java
@@ -2,17 +2,17 @@ package com.alevel.deliverit;
 
 import com.alevel.deliverit.customers.ParcelReceipt;
 import com.alevel.deliverit.customers.verticle.VertxContext;
+import com.alevel.deliverit.gateway.ByteArrayCodec;
+import com.alevel.deliverit.gateway.LogisticsVerticle;
 import com.google.gson.Gson;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.eventbus.MessageCodec;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
-
-import java.io.*;
 
 
 /**
@@ -23,60 +23,24 @@ import java.io.*;
  */
 public class RestVerticle extends AbstractVerticle {
 
+    public static void main(String[] args) {
+        VertxOptions vOpts = new VertxOptions();
+        vOpts.setClustered(true).setBlockedThreadCheckInterval(1000 * 60 * 60);
+        ;
+        Vertx.clusteredVertx(vOpts, cluster -> {
+            if (cluster.succeeded()) {
+                final Vertx result = cluster.result();
+                result.deployVerticle(new RestVerticle());
+                result.deployVerticle(new LogisticsVerticle());
+            }
+        });
+    }
+
     @Override
     public void start() throws Exception {
         VertxContext.instance().setEventBus(vertx.eventBus());
-        VertxOptions vertxOptions = new VertxOptions();
-        vertxOptions.setMaxEventLoopExecuteTime(Long.MAX_VALUE);
-
-        vertx.eventBus().registerCodec(new MessageCodec() {
-            @Override
-            public void encodeToWire(Buffer buffer, Object o) {
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                try (ObjectOutputStream out = new ObjectOutputStream(bos)) {
-                    out.writeObject(o);
-                    out.flush();
-                    final byte[] bytes = bos.toByteArray();
-                    buffer.appendBytes(bytes);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public Object decodeFromWire(int pos, Buffer buffer) {
-                ByteArrayInputStream bis = new ByteArrayInputStream(buffer.getBytes(pos, buffer.length()));
-                ObjectInput in = null;
-                try {
-                    in = new ObjectInputStream(bis);
-                    Object o = in.readObject();
-                    return o;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    return null;
-                }
-            }
-
-            @Override
-            public Object transform(Object o) {
-                return o;
-            }
-
-            @Override
-            public String name() {
-                return "myCodec";
-            }
-
-            @Override
-            public byte systemCodecID() {
-                return -1;
-            }
-        });
-        Vertx vertx = Vertx.vertx(vertxOptions);
-
-//        vertx.eventBus().registerCodec(new DefaultCodec());
-//        init();
+        vertx.eventBus().registerCodec(new ByteArrayCodec());
+        Vertx vertx = Vertx.vertx();
         HttpServer httpServer = vertx.createHttpServer();
         httpServer.requestHandler(request -> {
             Buffer fullRequestBody = Buffer.buffer();
@@ -86,11 +50,12 @@ public class RestVerticle extends AbstractVerticle {
                     String jsonRequest = buffer.toString();
                     System.out.println(jsonRequest.length());
 
-                    ParcelReceipt parcelReceipt = new ParcelReceptionEndpoint().accept(jsonRequest);
-                    String jsonResponse = new Gson().toJson(parcelReceipt);
-                    HttpServerResponse response = request.response();
-                    response.putHeader("content-type", "application/json; charset=utf-8");
-                    response.end(jsonResponse);
+                    new ParcelReceptionEndpoint().accept(jsonRequest, parcelReceipt -> {
+                        String jsonResponse = new Gson().toJson(parcelReceipt);
+                        HttpServerResponse response = request.response();
+                        response.putHeader("content-type", "application/json; charset=utf-8");
+                        response.end(jsonResponse);
+                    });
                 });
             }
         }).listen(8080);
